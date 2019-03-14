226f4b3625cee9f2e0209ba976dc835db0c30492
Rodrigo Ximenes <rodrigoximenes@tecgraf.puc-rio.br>
2019-03-14 13:20:04 -0300

    Falta injetar

---
+++ ExpensesSheet/Expenses.API/Controllers/EntriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Expenses.API.Controllers
+{
+    public class EntriesController : ApiController
+    {
+
+        public IHttpActionResult GetEntries()
+        {
+            return Ok();
+        }
+    }
+}
