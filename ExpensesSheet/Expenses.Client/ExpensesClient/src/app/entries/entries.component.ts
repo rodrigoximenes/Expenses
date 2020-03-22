dc955812ed9d3e8c284ce899b35f509247e5d62c
Rodrigo <Rodrigo>
2020-03-21 21:41:11 -0300

    Going Reactive

--- ExpensesSheet/Expenses.Client/ExpensesClient/src/app/entries/entries.component.ts
+++ ExpensesSheet/Expenses.Client/ExpensesClient/src/app/entries/entries.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EntryService } from '../entry.service';
-import { EntryElement } from '../Interfaces/EntryElement';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material';
 import { UpdateEntryComponent } from '../update-entry/update-entry.component';
 
@@ -13,16 +11,12 @@ import { UpdateEntryComponent } from '../update-entry/update-entry.component';
 export class EntriesComponent implements OnInit {
 
   displayedColumns: string[] = ['Description','IsExpense','Value','Actions']
-  dataSource;
+  dataSource = this.service.entries$;
 
   constructor(private service: EntryService,
     private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.service.getAll().subscribe((data) => {
-      console.log('Result -', data);
-      this.dataSource = new MatTableDataSource<EntryElement>(data as EntryElement[]);
-    });
   }
 
   updateEntry(entry){
@@ -34,8 +28,8 @@ export class EntriesComponent implements OnInit {
         IsExpense: entry.IsExpense,
         Value: entry.Value}
     });
-    dialogRef.afterClosed().subscribe(()=> this.service.getAll().subscribe((data) => {
-      console.log('Result -', data);
-      this.dataSource = new MatTableDataSource<EntryElement>(data as EntryElement[])}));
-  }
+  //   dialogRef.afterClosed().subscribe(()=> this.service.getAll().subscribe((data) => {
+  //     console.log('Result -', data);
+  //     this.dataSource = new MatTableDataSource<EntryElement>(data as EntryElement[])}));
+   }
 }
--- ExpensesSheet/Expenses.Client/ExpensesClient/src/app/entry.service.ts
+++ ExpensesSheet/Expenses.Client/ExpensesClient/src/app/entry.service.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject, combineLatest, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { EntryElement } from './Interfaces/EntryElement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntryService {
 
-  baseUrl: string = 'http://localhost:27123/api/entries/'
+  baseUrl: string = 'http://localhost:27123/api/entries/';
+
+  private updatedEntrydSubject = new Subject<EntryElement>();
+  updatedEntryAction$ = this.updatedEntrydSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  getEntry(id){
-    return this.http.get(this.baseUrl+'/'+id);
+  entries$ = this.http.get(this.baseUrl) as Observable<EntryElement>;
+
+  getEntry(id: number){
+    return this.http.get<EntryElement>(this.baseUrl+'/'+id);
   }
 
   getAll(){
-    return this.http.get(this.baseUrl);
+    return this.http.get<EntryElement[]>(this.baseUrl);
   }
 
-  createEntry(entry){
-    return this.http.post(this.baseUrl, entry);
+  createEntry(entry: EntryElement){
+    return this.http.post<EntryElement>(this.baseUrl, entry);
   }
 
-  updateEntry(id, entry){
-    return this.http.put(this.baseUrl+'/'+id,entry);
+  updateEntry(id: number, entry: EntryElement){
+    return this.http.put<EntryElement>(this.baseUrl+'/'+id,entry);
   }
 
-  deleteEntry(id){
-    return this.http.delete(this.baseUrl+'/'+id);
+  deleteEntry(id: number){
+    return this.http.delete<EntryElement>(this.baseUrl+'/'+id);
   }
 }
